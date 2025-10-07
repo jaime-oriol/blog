c1c280ed2bb9dd85222f46e6a3986b6b039f61d2
jaime-oriol <joriolgo@gmail.com>
2025-10-07 09:46:41 +0200

    new article

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,9 +2,27 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <div className="my-0.5 flex justify-center">
-    <NextImage src={`${basePath || ''}${src}`} {...rest} />
+interface CustomImageProps extends Omit<ImageProps, 'fill'> {
+  caption?: string
+}
+
+const Image = ({ src, caption, width, height, alt, ...rest }: CustomImageProps) => (
+  <div className="my-3 w-full">
+    <div className="relative w-full" style={{ aspectRatio: `${width}/${height}` }}>
+      <NextImage
+        src={`${basePath || ''}${src}`}
+        alt={alt}
+        fill
+        className="object-contain"
+        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 90vw, 1200px"
+        {...rest}
+      />
+    </div>
+    {caption && (
+      <p className="mt-2 text-center text-sm italic text-slate-600 dark:text-slate-400">
+        {caption}
+      </p>
+    )}
   </div>
 )
 
