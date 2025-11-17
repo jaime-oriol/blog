30d77b5cbc199889e9f55a839ad844b638fcde82
jaime-oriol <joriolgo@gmail.com>
2025-11-17 09:39:26 +0100

    fix videos

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -10,12 +10,12 @@ export default function VideoPlayer({ src, className = '' }: VideoPlayerProps) {
     <div className={`my-1.5 ${className}`}>
       {}
       <video
-        className="h-auto w-full rounded-lg object-contain"
+        className="h-auto w-full rounded-lg"
         controls
         muted
         playsInline
         preload="auto"
-        style={{ maxHeight: '800px', backgroundColor: '#000' }}
+        style={{ maxHeight: '500px', objectFit: 'cover' }}
         src={src}
       >
         Tu navegador no soporta el elemento de video.
